070aa305cf4393f35956ae91befb5a988cb025fc
Brandon <brosini9@gmail.com>
2021-08-14 02:04:52 -0400

    parse body

---
+++ generate.js
@@ -0,0 +1,7 @@
+
+function generate(req, res) {
+    const secret = req.body.secret;
+    //todo
+}
+
+module.exports = generate;
--- index.js
+++ index.js
@@ -4,14 +4,20 @@ require('dotenv').config();
 
 const port = process.env.PORT;
 
+const generate = require('./generate');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
+app.post('/generate', generate);
+/*
 app.post('/generate', (req, res) => {
     const back = req.header.Referer || '/';
     res.send(`todo<br/><a href=${back}>back</a>`);
 });
+*/
 
 app.listen(port, '127.0.0.1');
 console.log('listening at port: ' + port);
