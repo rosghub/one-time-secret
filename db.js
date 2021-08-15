e097e597861a338cf36266613234439e270f2962
brandon-desk <brosini9@gmail.com>
2021-08-15 12:12:54 -0400

    add connection timeout

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require('mongodb');
 
-const client = new MongoClient('mongodb://localhost:27017');
+const client = new MongoClient('mongodb://localhost:27017', {
+    serverSelectionTimeoutMS : process.env.DB_SERVER_TIMEOUT_MS
+});
 
 // return success
 module.exports.connectMongo = async () => {
+    console.log('Establing DB connection...');
     return client.connect().then(client => {
         console.log('Mongo connection successful')
         return true;
