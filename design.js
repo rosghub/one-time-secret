1d4d6a17e2ded2ccfec2a165c2d5f8863b326e05
Brandon <brosini9@gmail.com>
2021-08-16 00:04:51 -0400

    fix view long secret

--- design.js
+++ design.js
@@ -1,12 +1,14 @@
 // render EJS templates with mock data for page design
 
+const s = 'this is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secret';
+
 module.exports = () => {
     return (req, res, next) => {
         if (req.method == 'GET') {
             if (req.originalUrl == '/generate')
                 return res.render('generate', { link: 'link' });
             else if (req.originalUrl.startsWith('/view'))
-                return res.render('secret', { secret: 'secret' });
+                return res.render('secret', { secret: s });
         }
 
         next();
