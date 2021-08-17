6f728a0a31a0770c8e1692da3ff81d0626254cb4
Brandon <brosini9@gmail.com>
2021-08-17 11:27:13 -0400

    adjust viewsecret ejs spacing

--- design.js
+++ design.js
@@ -1,14 +1,12 @@
 // render EJS templates with mock data for page design
 
-const s = 'this is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secret';
-
 module.exports = () => {
     return (req, res, next) => {
         if (req.method == 'GET') {
             if (req.originalUrl == '/generate')
                 return res.render('generate', { link: 'link' });
             else if (req.originalUrl.startsWith('/view'))
-                return res.render('secret', { secret: s });
+                return res.render('secret', { secret: 'secret' });
         }
 
         next();
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { render } = require('ejs');
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
