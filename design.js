8d3954b0a14a0dbebe0b4a41568ef642cd0f2904
brandon-desk <brosini9@gmail.com>
2021-08-21 18:30:10 -0400

    fix link notification

--- design.js
+++ design.js
@@ -4,7 +4,7 @@ module.exports = () => {
     return (req, res, next) => {
         if (req.method == 'GET') {
             if (req.originalUrl == '/generate')
-                return res.render('generate', { link: 'thisisalonglinkthisisalonglinkthisisalonglinkthisisalonglinkthisisalonglinkthisisalonglink' });
+                return res.render('generate', { link: 'link' });
             else if (req.originalUrl.startsWith('/view'))
                 //return res.render('secret', { secret: 'this is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secretthis is a long secret' });
                 return res.render('decrypt', { link: '/view/1234', wrongPass: true });
