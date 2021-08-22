7702861d9063635cc80e722a454070fa7652b964
Brandon <brosini9@gmail.com>
2021-08-22 02:06:29 -0400

    top bar partial

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const app = express();
 app.set('trust proxy', '127.0.0.1');
 //app.use(require('./design')());
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => res.render('index'));
 app.post('/generate', generate);
 app.all('/view/:id', viewSecret);
 
