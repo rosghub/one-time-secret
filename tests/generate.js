64078d1c1b691223ce3247fc589fc7fe90ddd24b
Brandon <brosini9@gmail.com>
2021-08-28 03:02:05 -0400

    succesful link gen

--- tests/generate.js
+++ tests/generate.js
@@ -3,12 +3,15 @@ const chai = require('chai');
 const app = require('./../app');
 const constants = require('./../src/constants');
 const { parse } = require('node-html-parser');
+const { off } = require('./../app');
 
 chai.use(require('chai-http'));
 chai.should();
 
 describe('Test link generation', () => {
-    it('Generates link successfully', () => {
+    var link;
+
+    it('Generates link successfully', (done) => {
         chai.request(app).post('/generate')
             .type('form')
             .send({
@@ -18,12 +21,23 @@ describe('Test link generation', () => {
             })
             .end((err, res) => {
                 expect(err).to.be.null;
-                res.should.have.status(200);
+                expect(res).to.have.status(200);
 
+                // check link
                 const root = parse(res.text)
-                const link = root.querySelector('#secret').text;
-                console.log('Link: ' + link);
-                expect(link).to.be.a('string').and.match(/^http/);
+                link = root.querySelector('#secret').text.trim();
+                expect(link).to.be.a('string').and.match(/^https?:\/\/[^\/]+\/view\/\w+/);
+
+                // check ttl
+                const ttl = root.querySelector('#ttl').text.trim().split(' ')[0];
+                expect(ttl).to.be.equal('7');
+                done();
             });
     });
+
+/*
+    it('Generates link unsuccessfully', (done) => {
+
+    })
+*/
 });
