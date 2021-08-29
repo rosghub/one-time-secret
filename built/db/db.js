c9e2f4cc1773c61f84fc02b755a523cfdd89d445
brandon-desk <brosini9@gmail.com>
2021-08-28 21:09:38 -0400

    install typescript

---
+++ built/db/db.js
@@ -0,0 +1,104 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var MongoClient = require('mongodb').MongoClient;
+var _a = require('./../constants'), MONGO_URL = _a.MONGO_URL, MONGO_TABLE = _a.MONGO_TABLE, MONGO_INDEX_TTL = _a.MONGO_INDEX_TTL, DB_SERVER_TIMEOUT_MS = _a.DB_SERVER_TIMEOUT_MS;
+var client = new MongoClient(MONGO_URL, {
+    serverSelectionTimeoutMS: DB_SERVER_TIMEOUT_MS
+});
+var db = client.db(MONGO_TABLE);
+function connectDB() {
+    return __awaiter(this, void 0, void 0, function () {
+        var _this = this;
+        return __generator(this, function (_a) {
+            console.log('Establing DB connection...');
+            return [2 /*return*/, client.connect().then(function (client) { return __awaiter(_this, void 0, void 0, function () {
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0:
+                                console.log('Mongo connection successful');
+                                return [4 /*yield*/, checkIndexes()];
+                            case 1:
+                                _a.sent();
+                                return [2 /*return*/, {
+                                        success: true,
+                                        client: client
+                                    }];
+                        }
+                    });
+                }); }).catch(function (err) {
+                    console.error(err);
+                    console.log('Mongo connection failed');
+                    return {
+                        success: false,
+                        client: null
+                    };
+                })];
+        });
+    });
+}
+function checkIndexes() {
+    return __awaiter(this, void 0, void 0, function () {
+        var indexes, exists, name_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, client.db(MONGO_TABLE).collection('secrets').indexes()];
+                case 1:
+                    indexes = _a.sent();
+                    exists = indexes.find(function (e) { return e.name == MONGO_INDEX_TTL; });
+                    if (!!exists) return [3 /*break*/, 3];
+                    console.log('Index TTL missing, creating it now');
+                    return [4 /*yield*/, db.collection('secrets').createIndex({ expiresAt: 1 }, {
+                            expireAfterSeconds: 0,
+                            name: MONGO_INDEX_TTL
+                        })];
+                case 2:
+                    name_1 = _a.sent();
+                    if (name_1 == MONGO_INDEX_TTL)
+                        console.log('Index TTL created');
+                    return [3 /*break*/, 4];
+                case 3:
+                    console.log('Index TTL exists');
+                    _a.label = 4;
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+module.exports = {
+    db: db,
+    connectDB: connectDB
+};
---
+++ built/db/secrets.js
@@ -0,0 +1,58 @@
+var ObjectId = require('mongodb').ObjectId;
+var encrypt = require('../crypto').encrypt;
+var db = require('./db').db;
+var DEFAULT_SECRET_TTL = require('./../constants').DEFAULT_SECRET_TTL;
+function storeSecret(secret, password, ttl) {
+    var _ttl = ttl || DEFAULT_SECRET_TTL;
+    var _password = (password != null && password.length > 0) ? password : null;
+    // TTL days to ms
+    var expiresAt = new Date(new Date().getTime() + 86400000 * _ttl);
+    var doc = {
+        hash: encrypt(secret, _password),
+        userPass: _password != null,
+        expiresAt: expiresAt
+    };
+    return db.collection('secrets').insertOne(doc).then(function (res) {
+        var insertedId = res.insertedId;
+        console.log("Inserted secret " + insertedId + " with " + (password ? 'user' : 'default') + " pass.");
+        return {
+            insertedId: insertedId,
+            ttl: _ttl
+        };
+    }).catch(function (err) {
+        console.error(err);
+        return null;
+    });
+}
+function getSecret(id) {
+    try {
+        var collection = db.collection('secrets');
+        var doc = { _id: new ObjectId(id) };
+        return collection.findOne(doc).then(function (doc) {
+            if (doc) {
+                var hash = doc.hash;
+                doc.hash = {
+                    iv: hash.iv.buffer,
+                    message: hash.message.buffer,
+                    authTag: hash.authTag.buffer,
+                    salt: hash.salt.buffer
+                };
+                return doc;
+            }
+            return null;
+        });
+    }
+    catch (e) { }
+    return null;
+}
+function deleteSecret(id) {
+    var doc = { _id: new ObjectId(id) };
+    return db.collection('secrets').deleteOne(doc).then(function (res) {
+        console.log('Secret deleted');
+    });
+}
+module.exports = {
+    storeSecret: storeSecret,
+    getSecret: getSecret,
+    deleteSecret: deleteSecret
+};
